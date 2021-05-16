a82ae1114f1dbd9bb4353b7814f09671d0be8cc1
jelmiz <korhonen.valtteri@gmail.com>
2021-05-16 22:01:28 +0300

    eka

---
+++ SuperMario3Neat.py
@@ -0,0 +1,99 @@
+import retro
+import numpy as np
+import cv2
+import neat
+import pickle
+
+env = retro.make(game='SuperMarioBros3-Nes')
+
+imgarray = []
+
+def eval_genomes(genomes,config):
+
+    for genome_id, genome in genomes:
+        ob = env.reset()
+        ac = env.action_space.sample()
+
+        inx, iny, inc = env.observation_space.shape
+        inx = int(inx/8)
+        iny = int(iny/8)
+
+        net = neat.nn.recurrent.RecurrentNetwork.create(genome,config)
+
+        current_max_fitness = 0
+        fitness_current = 0
+        frame = 0
+        counter = 0
+        xpos = 0
+        jump = 0
+        xpos_max = 0
+        xpos_end = 1500
+        hyppy = True
+        done = False
+
+        while not done:
+
+            env.render()
+            frame += 1
+
+            ob = cv2.resize(ob, (inx, iny))
+            ob = cv2.cvtColor(ob, cv2.COLOR_BGR2GRAY)
+            #ob = np.reshape(ob, (inx, iny))
+
+            for x in ob:
+                for y in x:
+                    imgarray.append(y)
+            
+            nnOutput = net.activate(imgarray)
+            
+            ob, rew, done, info = env.step(nnOutput)
+
+            imgarray.clear()
+
+            xpos = info['mario_x_pos']
+            jump = info['inair']
+
+            if jump == 1 and hyppy == True:
+                fitness_current += 1
+                hyppy = False
+
+            if xpos == 0:
+                xpos_max = 0
+                fitness_current += 33500
+
+
+            if xpos > xpos_max:
+                fitness_current += 2
+                xpos_max = xpos
+                hyppy = True
+            
+            if xpos_max == xpos_end:
+                fitness_current += 100000
+                done = True
+
+            if fitness_current > current_max_fitness:
+                current_max_fitness = fitness_current
+                counter = 0
+            else:
+                counter += 1
+        
+            if done or counter == 200:
+                done = True
+                print(genome_id, fitness_current, xpos_max, xpos)
+
+            genome.fitness = fitness_current
+
+config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, 
+neat.DefaultStagnation, 'config-feedforward')
+
+p = neat.Population(config)
+
+p.add_reporter(neat.StdOutReporter(True))
+stats = neat.StatisticsReporter()
+p.add_reporter(stats)
+p.add_reporter(neat.Checkpointer(10))
+
+winner = p.run(eval_genomes)
+
+with open('winner.pkl', 'wb') as output:
+    pickle.dump(winner,output, 1)
